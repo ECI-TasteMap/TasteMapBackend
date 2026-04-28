1495f0e2bde4f0e5d62591d296a5cefadded0f9f
Waldron63 <ingsis.gualdron.santiago@gmail.com>
2026-04-28 13:17:06 -0500

    first commit

---
+++ src/test/java/com/eci/edu/ieti/tastemap/TastemapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.edu.ieti.tastemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
